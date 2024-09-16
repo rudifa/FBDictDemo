689b89de78567f476080f35e241da1992dd813fa
Rudi Farkas <rudi.farkas@gmail.com>
2024-09-16 23:30:39 +0200

    Initial Commit

---
+++ FBDictDemo/FBDictDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  FBDictDemoApp.swift
+//  FBDictDemo
+//
+//  Created by Rudolf Farkas on 16.09.24.
+//
+
+import SwiftUI
+
+@main
+struct FBDictDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
